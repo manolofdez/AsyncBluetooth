55dbaadb6245497b6550a6db569b1ca43e688c23
Manuel Fernandez <manolofdez@gmail.com>
2021-08-14 15:53:10 -0400

    Added Peripheral delegate wrapper.

--- Sources/Public/Peripheral.swift
+++ Sources/Public/Peripheral.swift
@@ -13,7 +13,39 @@ public struct Peripheral {
         self.cbPeripheral.identifier
     }
     
+    private lazy var cbPeripheralDelegate: CBPeripheralDelegate = {
+        CBPeripheralDelegateWrapper(
+            onDidReadRSSI: { rssi, error in },
+            onDidDiscoverServices: { error in },
+            onDidDiscoverIncludedServices: { service, error in },
+            onDidDiscoverCharacteristics: { service, error in },
+            onDidUpdateValueForCharacteristic: { characteristic, error in },
+            onDidWriteValueForCharacteristic: { characteristic, error in },
+            onDidUpdateNotificationState: { characteristic, error in },
+            onDidDiscoverDescriptors: { characteristic, error in },
+            onDidUpdateValueForDescriptor: { descriptor, error in },
+            onDidWriteValueForDescriptor: { descrioptor, error in },
+            onPeripheralIsReadyToSendWriteWithoutResponse: {},
+            onDidOpenChannel: { channel, error in }
+        )
+    }()
+    
     init(_ cbPeripheral: CBPeripheral) {
         self.cbPeripheral = cbPeripheral
     }
+    
+//    func readRSSI() {}
+//    func discoverServices(_ serviceUUIDs: [CBUUID]?) {}
+//    func discoverIncludedServices(_ includedServiceUUIDs: [CBUUID]?, for service: Service) {}
+//    func discoverCharacteristics(_ characteristicUUIDs: [CBUUID]?, for service: Service) {}
+//    func readValue(for characteristic: Characteristic) {}
+//    func maximumWriteValueLength(for type: CBCharacteristicWriteType) -> Int {}
+//    func writeValue(_ data: Data, for characteristic: Characteristic, type: CBCharacteristicWriteType) {}
+//    func setNotifyValue(_ enabled: Bool, for characteristic: Characteristic) {}
+//    func discoverDescriptors(for characteristic: Characteristic) {}
+//    func readValue(for descriptor: Descriptor) {}
+//    func writeValue(_ data: Data, for descriptor: Descriptor) {}
+//
+//    @available(iOS 11.0, *)
+//    func openL2CAPChannel(_ PSM: CBL2CAPPSM) {}
 }
