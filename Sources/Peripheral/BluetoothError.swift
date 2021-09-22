4b1e45b25efe70dc161f66d9eed60e987ed3fc1c
Manuel Fernandez <manolofdez@gmail.com>
2021-09-21 22:36:25 -0400

    Removing unused error.

--- Sources/Peripheral/BluetoothError.swift
+++ Sources/Peripheral/BluetoothError.swift
@@ -4,7 +4,6 @@ import Foundation
 
 public enum BluetoothError: Error {
     case bluetoothUnavailable
-    case scanningInProgress
     case connectingInProgress
     case disconnectingInProgress
     case errorConnectingToPeripheral(error: Error?)
