5b114bf6cdea6a8561eeff32f5028035ac52b436
Manuel Fernandez <manolofdez@gmail.com>
2023-07-09 13:16:15 -0400

    Introducing the flushable executor.

---
+++ Sources/Utils/FlushableExecutor.swift
@@ -0,0 +1,8 @@
+// Copyright (c) 2023 Manuel Fernandez. All rights reserved.
+
+import Foundation
+ 
+protocol FlushableExecutor where Self: Actor {
+    /// Sends an error to all queued and executing work.
+    func flush(error: Error) async throws
+}
