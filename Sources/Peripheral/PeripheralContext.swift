18bfc72271117db6e0511b29f42b34d8dc0795d6
Manuel Fernandez <manolofdez@gmail.com>
2023-11-04 09:59:51 -0400

    Add missing didModifyServices to peripheral delegate.

--- Sources/Peripheral/PeripheralContext.swift
+++ Sources/Peripheral/PeripheralContext.swift
@@ -7,6 +7,7 @@ import Combine
 /// Contains the objects necessary to track a Peripheral's commands.
 class PeripheralContext {
     private(set) lazy var characteristicValueUpdatedSubject = PassthroughSubject<Characteristic, Never>()
+    private(set) lazy var invalidatedServicesSubject = PassthroughSubject<[Service], Never>()
     
     private(set) lazy var readRSSIExecutor = {
         let executor = AsyncSerialExecutor<NSNumber>()
